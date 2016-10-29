89ee212f6f3ba372684cb970e07b5d9ae0615aa5
StephenPSA <psasoftware@outlook.com>
2016-10-29 21:39:09 +0200

    Updated Git functions

--- GitTools.ps1
+++ GitTools.ps1
@@ -15,14 +15,42 @@
 
 <#
 .Synopsis
-   (Will) Opens the GIT Cheat Sheet in Edge
+    Shows Wit Integration Status and help
+   Todo: Opens the GIT Cheat Sheet in Edge
 #>
-Function Show-GitHelp() {
+Function Show-GitQuickStart() {
+    # var
+    $hasGit = $false
+
+    # GIT Status
     try {
         $h = git --help
-        $h
+        $hasGit = $true
+        # Cue User
+        Write-Host "Git is installed" -ForegroundColor DarkGreen
     }
     catch {
+        # Cue User
         Write-Host "Git is not installed" -ForegroundColor Red
+        Write-Host "Install Git from: https://git-scm.com/downloads" 
     }
+
+    # Posh GIT Status
+    $pg = Get-Module -Name posh-git -ErrorAction SilentlyContinue
+    if( $pg -eq $null ) {
+        # Cue User
+        Write-Host "Posh Git is not installed" -ForegroundColor Red
+        Write-Host "Install Posh-Git from: https://github.com/dahlbyk/posh-git" 
+    }
+    else {
+        # Cue User
+        Write-Host "Posh Git is installed: Version: $($pg.Version)" -ForegroundColor DarkGreen
+    }
+
+    # Show Cheat Sheet
+    Write-Host "See Git Cheat Sheet at: https://services.github.com/kit/downloads/github-git-cheat-sheet.pdf" 
+
+    # EOF
 }
+
+# EOS
