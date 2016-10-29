fd67a00bea14ebcf40610a177624dade4ae1a165
StephenPSA <psasoftware@outlook.com>
2016-10-29 20:35:09 +0200

    Version 0.0.6.6

---
+++ GitTools.ps1
@@ -0,0 +1,28 @@
+﻿##=================================================================================================
+# File    : GitTools.ps1
+# Author  : StephenPSA
+# Version : 0.0.6.6
+# Date    : Oct, 2016
+#
+# Defines Funcions connected to Git use
+#
+# See:
+#    Git Cheat Sheet                 - https://services.github.com/kit/downloads/github-git-cheat-sheet.pdf
+#    Git install                     - https://git-scm.com/downloads
+#    Git in PowerShell (ISE) module  - https://github.com/dahlbyk/posh-git
+##-------------------------------------------------------------------------------------------------
+#requires -Version 5.0
+
+<#
+.Synopsis
+   (Will) Opens the GIT Cheat Sheet in Edge
+#>
+Function Show-GitHelp() {
+    try {
+        $h = git --help
+        $h
+    }
+    catch {
+        Write-Host "Git is not installed" -ForegroundColor Red
+    }
+}
--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-# WindowsInsiderTools - BETA 0.0.6.5
+# WindowsInsiderTools - BETA 0.0.6.6
 # Under construction, use at own risk
 Handy PowerShell Module for Windows Insider members
 
 + Nov, 2016
 
++ Git and PowerShell ISE : 0.0.6.6
 + GitHub support         : 0.0.5.0
 + Initial Population with: 0.0.4.x
 
