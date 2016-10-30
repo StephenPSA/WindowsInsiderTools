7d38c053e545a2f3d45d2e115d569f14ea779baa
StephenPSA <psasoftware@outlook.com>
2016-10-30 19:58:38 +0100

    Version 0.0.6.17

---
+++ Tools/ContributerTools.ps1
@@ -0,0 +1,12 @@
+﻿##=================================================================================================
+# File    : ContributerTools.ps1
+# Author  : StephenPSA
+# Version : 0.0.6.17
+# Date    : Nov, 2016
+#
+# Defines Funcions for WindowsInsiderTools contributers
+#
+##-------------------------------------------------------------------------------------------------
+#requires -Version 5.0
+
+# EOS
