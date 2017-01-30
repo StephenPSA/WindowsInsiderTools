0b6adbd3e9dd244784e606240a853c59b3c922ca
StephenPSA <psasoftware@outlook.com>
2017-01-30 03:27:20 +0100

    typo fix

--- FooScrip.ps1
+++ FooScrip.ps1
@@ -1,10 +1,13 @@
 ﻿# GIT Refresher
 
-git pull 'https://github.com/StephenPSA/WindowsInsiderTools'
+# git pull 'https://github.com/StephenPSA/WindowsInsiderTools'
 
+# git remote add 'repo' 'https://github.com/StephenPSA/WindowsInsiderTools'
+
+# git branch 'V-0-0-10-9'
+
+# -- make edits ---
+# git add *
 # git commit -m 'No message'
 
-
-
-
-# git push origin -u 'https://github.com/StephenPSA/WindowsInsiderTools'
+# git push --set-upstream repo V-0-0-10-1
