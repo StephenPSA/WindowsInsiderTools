9b7686819803cd07d7453652f7b491557b984998
StephenPSA <psasoftware@outlook.com>
2017-01-30 03:12:50 +0100

    New version

---
+++ FooScrip.ps1
@@ -0,0 +1,10 @@
+﻿# GIT Refresher
+
+git pull 'https://github.com/StephenPSA/WindowsInsiderTools'
+
+# git commit -m 'No message'
+
+
+
+
+# git push origin -u 'https://github.com/StephenPSA/WindowsInsiderTools'
