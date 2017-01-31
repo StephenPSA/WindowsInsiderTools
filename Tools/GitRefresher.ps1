e9e8c0ffdd1e75f424cd3d4bca494f275c1c4c1d
StephenPSA <psasoftware@outlook.com>
2017-01-31 01:20:36 +0100

    First capabilities (Read-Only)

--- Tools/GitRefresher.ps1
+++ Tools/GitRefresher.ps1
@@ -4,10 +4,12 @@
 
 # git remote add 'repo' 'https://github.com/StephenPSA/WindowsInsiderTools'
 
-# git branch 'V-0-0-10-9'
+# git branch 'V-0-0-10-1'
 
 # -- make edits ---
 # git add *
+
+# NOTE: 'git commit' without the -m option -> ERROR
 # git commit -m 'No message'
 
 # git push --set-upstream repo V-0-0-10-1
